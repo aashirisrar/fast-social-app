12b6c733337d2993b663dcc49495aa373a0ea193
Amaz Ahmed <l226837@lhr.nu.edu.pk>
2024-03-13 14:41:37 +0500

    abcd

--- components/hero.tsx
+++ components/hero.tsx
@@ -62,7 +62,13 @@ const Hero = () => {
           href="/components"
         >
           Components
+        </a><a
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 rounded-[6px]"
+          href="/components"
+        >
+          Components
         </a>
+        
       </div>
     </section>
   );

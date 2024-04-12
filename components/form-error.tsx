c54257647a03ca150ea5d68ebca2bebbfabe4d10
John Doe <aashirisrar968@gmail.com>
2024-04-12 11:40:18 +0500

    setting up the authentication functionality

--- components/form-error.tsx
+++ components/form-error.tsx
@@ -10,7 +10,7 @@ export const FormError = ({ message }: FormErrorProps) => {
   return (
     <div
       className="bg-destructive/15 p-3 rounded-md flex
-        items-center gap-x-2 text-sm text-destructive"
+        items-center gap-x-2 text-sm text-red-600"
     >
       <AlertCircle size={16} />
       {message}

0cc2f92990833627e712c1669e0cc0618f79bc30
John Doe <aashirisrar968@gmail.com>
2024-04-12 23:55:43 +0500

    fixed the settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,12 +2,15 @@ import Link from "next/link";
 import {
     Bell,
     CircleUser,
+
     Home,
     LineChart,
     Menu,
     Package,
     Package2,
+    Pen,
     Search,
+    Settings,
     ShoppingCart,
     Users,
 } from "lucide-react";
@@ -52,11 +55,11 @@ export default function Dashboard() {
                     <div className="flex-1">
                         <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
                             <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                                href="/settings"
+                                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
                             >
-                                <Home className="h-4 w-4" />
-                                Dashboard
+                                <Pen className="h-4 w-4" />
+                                User Profile
                             </Link>
                             <Link
                                 href="#"
@@ -68,44 +71,7 @@ export default function Dashboard() {
                                     6
                                 </Badge>
                             </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                            >
-                                <Package className="h-4 w-4" />
-                                Products{" "}
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                            >
-                                <Users className="h-4 w-4" />
-                                Customers
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                            >
-                                <LineChart className="h-4 w-4" />
-                                Analytics
-                            </Link>
                         </nav>
-                    </div>
-                    <div className="mt-auto p-4">
-                        <Card x-chunk="dashboard-02-chunk-0">
-                            <CardHeader className="p-2 pt-0 md:p-4">
-                                <CardTitle>Upgrade to Pro</CardTitle>
-                                <CardDescription>
-                                    Unlock all features and get unlimited access to our support
-                                    team.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                                <Button size="sm" className="w-full">
-                                    Upgrade
-                                </Button>
-                            </CardContent>
-                        </Card>
                     </div>
                 </div>
             </div>

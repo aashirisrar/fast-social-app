58896c4ebcf65f70217e73a9a2366056b08959b7
John Doe <aashirisrar968@gmail.com>
2024-04-16 21:23:34 +0500

    added the functionality to add friends

--- components/add-friends.tsx
+++ components/add-friends.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import axios from "axios"
+import { useState } from "react"
 
 const AddFriendComponent = ({ firstName, lastName, name, profilePicture }: any) => {
+    const [isFriendAdded, setIsFriendAdded] = useState(false);
+
+    const addFriend = async () => {
+        try {
+            const response = await axios.post('/api/addfriend', { username: name });
+            setIsFriendAdded(true);
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
         <div className="w-full bg-muted text-primary rounded-xl" key={name}>
             <Image
@@ -16,10 +30,11 @@ const AddFriendComponent = ({ firstName, lastName, name, profilePicture }: any) 
                 <div className="text-md font-medium">{firstName + " " + lastName}</div>
                 <div className="text-sm text-muted-foreground font-medium mb-2">{name}</div>
                 <div className="flex flex-col gap-1 items-center">
-                    <Button className="w-full" variant="outline"
-                    >
-                        Add Friend
-                    </Button>
+                    {!isFriendAdded && ( // Render button only if friend not added
+                        <Button onClick={addFriend} className="w-full" variant="outline">
+                            Add Friend
+                        </Button>
+                    )}
                     <Link href={"/user/" + name}>
                         <Button className="w-full" variant="outline"
                         >

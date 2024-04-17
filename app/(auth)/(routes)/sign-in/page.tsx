dc182da56f9fb2c08aa06cbc9a0f3d5684587107
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2024-04-17 23:41:03 +0500

    Update Sign-In

--- app/(auth)/(routes)/sign-in/page.tsx
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -11,9 +11,9 @@ export default function Dashboard() {
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
+            <h1 className="text-3xl font-bold">FAST-SOCIALS</h1>
             <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
+              Login an account
             </p>
           </div>
           <div className="grid gap-4">
@@ -22,27 +22,32 @@ export default function Dashboard() {
               <Input
                 id="email"
                 type="email"
-                placeholder="lXX-XXXX@lhr.nu.edu.pk"
+                placeholder="XYZ@gmail.com"
                 required
               />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
               </div>
-              <Input id="password" type="password" required />
+              <div style={{ display: "flex" }}>
+                <Input id="password" type="password" required />
+              </div>
+              <Link
+                href="/forgot-password"
+                className="ml-auto inline-block text-sm "
+              >
+                Forgot password?
+              </Link>
             </div>
             <Button type="submit" className="w-full">
               Login
             </Button>
+            <Button variant="outline" className="w-full">
+              Login with Google
+            </Button>
           </div>
-          <div className="mt-4 text-center text-sm">
+          <div className="text-center text-sm mt-20">
             Don&apos;t have an account?{" "}
             <Link href="/sign-up" className="underline">
               Sign up

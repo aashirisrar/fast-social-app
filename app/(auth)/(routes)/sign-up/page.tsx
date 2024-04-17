3fb6535db7b883510c43e3ed5e57b8096bf2a6d2
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2024-04-17 23:41:44 +0500

    Update Sign-Up

--- app/(auth)/(routes)/sign-up/page.tsx
+++ app/(auth)/(routes)/sign-up/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { Textarea } from "@nextui-org/react";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -5,20 +8,56 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { SignIn } from "@/components/sign-up";
-
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function LoginForm() {
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
         <CardTitle className="text-xl">Sign Up</CardTitle>
-        <CardDescription>
-          Enter your information to create an account
-        </CardDescription>
+        <CardDescription>Create an account</CardDescription>
       </CardHeader>
       <CardContent>
-        <SignIn />
+        <div className="grid gap-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="first-name">First name</Label>
+              <Input id="first-name" placeholder="First Name" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="last-name">Last name</Label>
+              <Input id="last-name" placeholder="Last Name" required />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="bio">Bio</Label>
+            <Textarea placeholder="Type your BIO here" id="bio" />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="gender">Gender</Label>
+              <select name="gender" id="genders">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="mix">Rather not to say</option>
+              </select>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="dob">DOB</Label>
+              <Input type="date" name="dob" style={{ width: "fit-content" }} />
+            </div>
+          </div>
+          <Button type="submit" className="w-full mt-10">
+            Create an account
+          </Button>
+        </div>
+        <div className="mt-20 text-center text-sm">
+          Already have an account?{" "}
+          <Link href="/sign-in" className="underline">
+            Sign in
+          </Link>
+        </div>
       </CardContent>
     </Card>
   );

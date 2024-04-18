56a8d8e5151cf0a31a0f993116eb3ea8651a2f87
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2024-04-18 10:30:58 +0500

    Update post-item.tsx

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -9,9 +9,18 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 
-export default function PostComponent({ userId, body, image, likeCount, commentCount, postId, createdAt }: any) {
-
-  function calculateTimeDifference(timeCreatedAt: Date | string | number): string {
+export default function PostComponent({
+  userId,
+  body,
+  image,
+  likeCount,
+  commentCount,
+  postId,
+  createdAt,
+}: any) {
+  function calculateTimeDifference(
+    timeCreatedAt: Date | string | number
+  ): string {
     const postTime: Date = new Date(timeCreatedAt); // Convert to Date object
 
     const currentTime: Date = new Date();
@@ -29,37 +38,40 @@ export default function PostComponent({ userId, body, image, likeCount, commentC
       return "Just now";
     } else if (seconds < hour) {
       const minutes = Math.floor(seconds / minute);
-      return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+      return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
     } else if (seconds < day) {
       const hours = Math.floor(seconds / hour);
-      return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+      return `${hours} hour${hours > 1 ? "s" : ""} ago`;
     } else {
       // For differences greater than a day, displaying the actual date and time
       return postTime.toLocaleString();
     }
   }
 
-  const [userName, setUserName] = useState('');
-  const [imageSrc, setImageSrc] = useState('');
-  const [name, setName] = useState('');
+  const [userName, setUserName] = useState("");
+  const [imageSrc, setImageSrc] = useState("");
+  const [name, setName] = useState("");
 
   // get username of the user who posted the post
   useEffect(() => {
     async function fetchUserNames() {
       try {
-        const response = await axios.post('/api/post/getusername', { userId: userId });
+        const response = await axios.post("/api/post/getusername", {
+          userId: userId,
+        });
         setUserName(response.data.user.name);
         setImageSrc(response.data.user.profilePicture);
         // for setting the fallback of avatar
-        setName(response.data.user.firstName[0] + response.data.user.lastName[0]);
+        setName(
+          response.data.user.firstName[0] + response.data.user.lastName[0]
+        );
       } catch (error) {
-        console.error('Error incrementing likes:', error);
+        console.error("Error incrementing likes:", error);
       }
     }
 
     fetchUserNames();
   }, []);
-
 
   return (
     <Card className="space-y-2 px-6 mb-4 w-[650px]">
@@ -71,7 +83,9 @@ export default function PostComponent({ userId, body, image, likeCount, commentC
           </Avatar>
           <div className="grid gap-1">
             <Link href={"/user/" + userName}>
-              <p className="text-sm font-medium leading-none hover:underline">{userName}</p>
+              <p className="text-sm font-medium leading-none hover:underline">
+                {userName}
+              </p>
             </Link>
             <p className="text-sm text-muted-foreground">
               {calculateTimeDifference(createdAt)}
@@ -82,17 +96,26 @@ export default function PostComponent({ userId, body, image, likeCount, commentC
       </CardHeader>
       <CardContent className="space-y-4">
         <div className="flex flex-col items-start gap-4">
-          <p className="text-sm text-muted-foreground">{body}
-          </p>
+          <p className="text-sm text-muted-foreground">{body}</p>
           <div className="bg-red-500 w-full">
-            <Image width={400} style={{ objectFit: "contain" }} height={400} alt={postId} src={image} />
+            <Image
+              width={400}
+              className="w-full"
+              style={{ objectFit: "contain" }}
+              height={400}
+              alt={postId}
+              src={image}
+            />
           </div>
         </div>
         <div className="flex gap-2">
           {/* add like count here */}
           <LikeComponent postId={postId} likeCount={likeCount} />
           {/* add comment count here */}
-          <Button variant="outline"><MessageCircle size={16} className="mr-1" />{commentCount}</Button>
+          <Button variant="outline">
+            <MessageCircle size={16} className="mr-1" />
+            {commentCount}
+          </Button>
         </div>
       </CardContent>
     </Card>

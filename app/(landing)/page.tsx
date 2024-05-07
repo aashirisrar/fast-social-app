06a098b4716170471a5b09eda1f02d30ea06a346
John Doe <aashirisrar968@gmail.com>
2024-05-07 09:51:03 +0500

    fixed colors

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import {Cta,Navbar,Brand} from '@/components/index'
-import { Footer,Blog,Header,Possibility,Whatgpt3,Features } from '@/components/Landingcomps/index'
- import './App.css'
+import { Cta, Navbar, Brand } from '@/components/index'
+import { Footer, Blog, Header, Possibility, Whatgpt3, Features } from '@/components/Landingcomps/index'
+import './App.css'
 const Home = () => {
   return (
-    
-    <div className='App'>
-       <div className='gradient__bg'>
-       <Navbar />
-       <Header />
-       </div>
-       <Brand />
-       <Whatgpt3 />
-       <Features />
-       <Possibility />
-       
-       <Cta />
-       <Blog />
-       <Footer />
+
+    <div className='App bg-[#040C18]'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Whatgpt3 />
+      <Features />
+      <Possibility />
+
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   )
 }

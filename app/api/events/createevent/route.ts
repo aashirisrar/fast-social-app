f8c62790b9b74d999926065128ff83749418d003
John Doe <aashirisrar968@gmail.com>
2024-05-08 19:31:58 +0500

    fixed the event component

--- app/api/events/createevent/route.ts
+++ app/api/events/createevent/route.ts
@@ -30,6 +30,7 @@ export async function POST(req: Request) {
                 details,
                 image,
                 userId: currentUser?.id!,
+                date: new Date(date)
             },
         })
 
--- components/event-display.tsx
+++ components/event-display.tsx
@@ -14,30 +14,31 @@ export const EventDisplayComponent = ({ event }: any) => {
               <AvatarImage src={event.user.profilePicture} alt="Avatar" />
               <AvatarFallback>OM</AvatarFallback>
             </Avatar>
-            <div>{event.user.name}</div>
-            <CardTitle style={{ marginTop: "30px", marginLeft: "10px" }}>
+            <CardTitle style={{ marginTop: "20px", marginLeft: "10px" }}>
               {event.user.firstName}
+              <div className="text-sm font-light">@ {event.user.name}</div>
             </CardTitle>
           </div>
-          <div className="text-center text-xl">{event.name}</div>
+          <div className="text-center text-xl font-bold">{event.name}</div>
           <div className="flex mt-[25px] ">
             {/* <p className="text-sm font-medium leading-none">{}</p> */}
             <div className="flex-1 w-[30%]">
-              <img src={event.image} alt="EventImg" />
+              <img src={event.image} className="mx-auto" alt="EventImg" />
             </div>
             <div className="flex-2 w-[70%]">
-              <p
-                className="text-sm text-muted-foreground"
-                style={{ textAlign: "center" }}
-              >
-                {event.location}
-              </p>
-              <p>{event.date}</p>
+              <div className="grid grid-cols-2 text-center justify-center items-center h-full">
+                <div>
+                  {event.location}
+                </div>
+                <div>
+                  {event.date}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="mx-auto font-medium">Details: {event.details}</div>
+          <div className="mx-auto font-medium mt-8">{event.details}</div>
         </div>
       </CardContent>
-    </Card>
+    </Card >
   );
 };
